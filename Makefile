c9311dff2e9e7a292442c91d40178e7775b534e1
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-15 15:41:45 -0500

    Initial commit

---
+++ Makefile
@@ -0,0 +1,34 @@
+SHELL := /bin/bash
+
+MAKEFILES := $(shell find . -name "Makefile" | grep -E "^./[^/]*/Makefile\$$")
+MAKEDIRS := $(patsubst ./%, %, $(patsubst %/Makefile, %, $(MAKEFILES)))
+BUILD_CMDS := $(patsubst %, build-%, $(MAKEDIRS))
+CLEAN_CMDS := $(patsubst %, clean-%, $(MAKEDIRS))
+
+.DEFAULT_GOAL := help
+
+.PHONY: $(BUILD_CMDS)
+$(BUILD_CMDS):
+	$(info $(filter %/$(strip $(patsubst build-%, %, $@)), $(MAKEDIRS)) build)
+	@make -C $(filter %/$(strip $(patsubst build-%, %, $@)), $(MAKEDIRS)) build
+
+.PHONY: build
+build: $(BUILD_CMDS)
+
+.PHONY: clean
+clean: $(CLEAN_CMDS)
+
+
+.PHONY: help
+help:
+	@echo "Detected sub-dir Makefiles:"
+	@echo $(MAKEFILES) | tr [:space:] "\n"
+	@echo
+	@echo "Available build commands:"
+	@echo "build"
+	@echo $(BUILD_CMDS) | tr [:space:] "\n"
+	@echo
+	@echo "Available clean commands:"
+	@echo "clean"
+	@echo $(CLEAN_CMDS) | tr [:space:] "\n"
+	@echo
---
+++ common/build.mk
@@ -0,0 +1,52 @@
+SHELL := /bin/bash
+
+PREFIX = tgsmith61591
+
+GIT_HASH = $(shell git rev-parse --verify HEAD | cut -c 1-7)
+GIT_TAG = $(shell git describe --tags --exact-match $(GIT_HASH) 2>/dev/null)
+
+IMAGE = $(PREFIX)/$(IMAGE_NAME)
+LOCAL = $(IMAGE):local
+LATEST = $(IMAGE):latest
+HASHED = $(IMAGE):ci-$(GIT_HASH)
+TAGGED = $(IMAGE):$(GIT_TAG)
+
+
+.PHONY: logmsg
+logmsg:
+	$(info # ##############################)
+	$(info #)
+	$(info # Now building $(IMAGE_NAME))
+	$(info #)
+	$(info # ##############################)
+
+
+.PHONY: build
+build: logmsg
+	docker build \
+		--cache-from $(LATEST) \
+		-t $(LOCAL) .
+	docker tag $(LOCAL) $(HASHED)
+
+.PHONY: push-hashed
+push-hashed:
+ifndef CIRCLECI
+	$(error Pushing is only intended to be used from within the Circle CI workflow)
+else
+	docker push $(HASHED)
+endif
+
+.PHONY: pull-hashed
+pull-hashed:
+	docker pull $(HASHED)
+	docker tag $(HASHED) $(LOCAL)
+
+.PHONY: pull-latest
+pull-latest:
+	docker tag $(LATEST) $(LATEST)-$(REVISION) || true
+	docker pull $(LATEST) || true
+
+.PHONY: pull-base
+pull-base:
+	docker pull $(BASE)
+	docker tag $(BASE) $(BASE_LOCAL)
