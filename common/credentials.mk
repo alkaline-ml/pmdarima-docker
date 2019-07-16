bed1e54c9a5a314b6aca825c08c36cb9cc2ad8be
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-15 21:00:34 -0500

    Buh

--- common/credentials.mk
+++ common/credentials.mk
@@ -2,4 +2,5 @@ SHELL := /bin/bash
 
 .PHONY: docker-login
 docker-login:
-	env | grep DOCKER_PASSWORD | awk '{split($0,a,"="); print a[2]}' | docker login -u=${DOCKER_USERNAME} --password-stdin
+	env | grep DOCKER_PASSWORD | awk '{split($0,a,"="); print a[2]}' > pwd.cred
+	cat pwd.cred | docker login -u=${DOCKER_USERNAME} --password-stdin
