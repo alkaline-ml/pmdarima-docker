d86eda5de7844e07fb66e6dd2003889d551acca3
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-15 20:57:02 -0500

    :hackerman:

--- common/credentials.mk
+++ common/credentials.mk
@@ -2,5 +2,4 @@ SHELL := /bin/bash
 
 .PHONY: docker-login
 docker-login:
-	echo ${DOCKER_PASSWORD} > pw.txt
-	cat pw.txt | docker login -u=${DOCKER_USERNAME} --password-stdin
+	env | grep DOCKER_PASSWORD | awk '{split($0,a,"="); print a[2]}' | docker login -u=${DOCKER_USERNAME} --password-stdin
