1e45125b874a08e16cd7f354878c19a63df60202
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-15 21:01:45 -0500

    Fix awk?

--- common/credentials.mk
+++ common/credentials.mk
@@ -2,5 +2,6 @@ SHELL := /bin/bash
 
 .PHONY: docker-login
 docker-login:
-	env | grep DOCKER_PASSWORD | awk '{split($0,a,"="); print a[2]}' > pwd.cred
+	# To be used on Circle only
+	env | grep DOCKER_PASSWORD | awk '{split($$0,a,"="); print a[2]}' > pwd.cred
 	cat pwd.cred | docker login -u=${DOCKER_USERNAME} --password-stdin
