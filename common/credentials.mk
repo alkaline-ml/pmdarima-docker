4109a8ee47f99fc420517f5a881fd0f538059868
tgsmith61591 <tgsmith61591@gmail.com>
2019-07-15 20:52:50 -0500

    Docker pls login?

--- common/credentials.mk
+++ common/credentials.mk
@@ -2,5 +2,5 @@ SHELL := /bin/bash
 
 .PHONY: docker-login
 docker-login:
-	echo $DOCKER_PASSWORD > pw.txt
+	echo ${DOCKER_PASSWORD} > pw.txt
 	cat pw.txt | docker login -u=${DOCKER_USERNAME} --password-stdin
